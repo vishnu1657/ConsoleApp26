5c32825fadb8fc83932e96dde784fa0c1a689464
HP <HP@DESKTOP-4Q8V1JF>
2023-03-01 05:46:52 +0530

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public class program
+{
+    public static void Main()
+
+    {
+        //calcuate the vowels from the string
+
+        Console.WriteLine("please enter any string");
+        var str = Console.ReadLine(); //welcome
+
+        int vowelscounter = 0;
+        foreach (var item in str) 
+        {
+           if (item == 'a'|| item == 'e'|| item == 'i'|| item == 'o'|| item == 'u'||
+               item == 'a'||  item == 'e' || item == 'i' || item == 'o'|| item == 'u')  
+                
+           {
+               vowelscounter++;
+           }
+        }
+        Console.WriteLine("number of vowels in the string=" + vowelscounter);
+
+
+
+    }
+}
+
+    
